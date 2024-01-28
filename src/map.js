95992dedddc6d18980ccb3f5a54b909004ee3a0b
Gabe Lacroix <119274914+gabri015@users.noreply.github.com>
2024-01-28 11:53:49 -0500

    Create map.js

---
+++ src/map.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+function MyComponent(props) {
+  return (
+    <img
+      loading="lazy"
+      srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/aafb6117f32dfd7120163021050105b02a53888cbfd462d384bd38c6915278a2?apiKey=dacf4dc538274d118514255101e4e2b4&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/aafb6117f32dfd7120163021050105b02a53888cbfd462d384bd38c6915278a2?apiKey=dacf4dc538274d118514255101e4e2b4&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/aafb6117f32dfd7120163021050105b02a53888cbfd462d384bd38c6915278a2?apiKey=dacf4dc538274d118514255101e4e2b4&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/aafb6117f32dfd7120163021050105b02a53888cbfd462d384bd38c6915278a2?apiKey=dacf4dc538274d118514255101e4e2b4&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/aafb6117f32dfd7120163021050105b02a53888cbfd462d384bd38c6915278a2?apiKey=dacf4dc538274d118514255101e4e2b4&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/aafb6117f32dfd7120163021050105b02a53888cbfd462d384bd38c6915278a2?apiKey=dacf4dc538274d118514255101e4e2b4&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/aafb6117f32dfd7120163021050105b02a53888cbfd462d384bd38c6915278a2?apiKey=dacf4dc538274d118514255101e4e2b4&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/aafb6117f32dfd7120163021050105b02a53888cbfd462d384bd38c6915278a2?apiKey=dacf4dc538274d118514255101e4e2b4&"
+      className="aspect-[0.46] object-contain object-center w-full max-w-[480px] mx-auto"
+    />
+  );
+}
+
